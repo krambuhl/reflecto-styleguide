1f1fe377beda568d347e077f8473c31ddc257c54
Evan Krambuhl <thekrambuhl@gmail.com>
2017-09-29 10:50:00 -0700

    add styleguide config defaults

--- assets.js
+++ assets.js
@@ -4,5 +4,39 @@ const path = require('path')
 const css = fs.readFileSync(path.resolve(__dirname, 'dist/bundle.css'))
 const js = fs.readFileSync(path.resolve(__dirname, 'dist/bundle.js'))
 
-module.exports.css = ({ fonts, colors }) => css
-module.exports.js = ({ fonts, colors }) => js
+const getConfig = ({ fonts, colors }) => {
+  return {
+    fonts: Object.assign({
+      body: `"Helvetica Neue", "Helvetica", "Segoe UI", sans-serif`,
+      headers: `"Helvetica Neue", "Helvetica", "Segoe UI", sans-serif`,
+      code: `"Andale Mono", "Lucida Console", mono`
+    }, fonts),
+    colors: Object.assign({
+      background: '#fff',
+      text: '#666',
+      active: '#4925a0',
+      inlineCode: '#4925a0',
+      tableHeaders: '#000',
+      tableRowBorder: '#999',
+      headerBackground: '#fff',
+      headerTitle: '#999',
+      headerLinks: '#666',
+      headerBorder: '#ddd',
+      exampleHeaderBackground: '#222',
+      exampleHeaderForeground: '#fff',
+      codeHeaderBackground: '#4925a0',
+      codeHeaderForeground: '#fff',
+      codeBackground: '#090226'
+    }, colors)
+  }
+}
+
+module.exports.css = (config) => {
+  const { fonts, colors } = getConfig(config)
+  return css
+}
+
+module.exports.js = (config) => {
+  const { fonts, colors } = getConfig(config)
+  return js
+}
